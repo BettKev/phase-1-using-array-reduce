d6c07f11bce3c4114ae7bcc1a20c5ed117f86d4d
Kevin Bett <prodigy4614@gmail.com>
2024-10-04 18:14:37 +0300

    update reduce function

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
-   
-// Array of battery counts
-// const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
-
 // Using reduce to sum the total number of batteries
 const totalBatteries = batteryBatches.reduce((accumulator, currentBatch) => {
   return accumulator + currentBatch;
